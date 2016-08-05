de9343489363aab9ebe550360875cf2219da452a
MacLane Wilkison <mwilkison@gmail.com>
2016-08-05 15:03:41 -0700

    Remove other auto-generated stuff

--- zerodb_dbaas/models.py
+++ zerodb_dbaas/models.py
@@ -1,17 +1,6 @@
 from zerodb.models import Model, Field
 
 
-class Counter(Model):
-    name = Field()
-    count = Field()
-
-    def __repr__(self):
-        return "<Counter %s %s>" % (self.name, self.count)
-
-    def inc(self):
-        self.count += 1
-
-
 def make_app(db):
     num = len(db[Counter])
     if num < 1:
