61290b1919c7f953f2ceb3875876de803a14648d
Michael Egorov <michael@zerodb.io>
2016-08-06 13:45:45 -0700

    JSON API to add (unconfirmed) subdb

--- zerodb_dbaas/views/instances.py
+++ zerodb_dbaas/views/instances.py
@@ -1,6 +1,10 @@
+from persistent.mapping import PersistentMapping
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
 from zerodb_dbaas.views.common import humansize
+from zerodb_dbaas.models import UserRegistration
+
+from .common import decode_password_hex
 
 
 def manage_databases(request):
@@ -54,3 +58,43 @@ def instance(request):
                 size = admin.user_stats[username]
 
     return {'username': username, 'size': humansize(size)}
+
+
+@view_config(route_name='add_subdb', renderer='json',
+             effective_principals=['group:customers'])
+def add_subdb(request):
+    db = request.dbsession
+    form = request.params
+
+    email = request.authenticated_userid
+    instance_postfix = form.get('instance_postfix')
+    if instance_postfix:
+        username = '-'.join([email, instance_postfix])
+    else:
+        username = email
+
+    password = form.get('password')
+    password_hash = decode_password_hex(password)
+    # Also need some stripe things
+
+    user = db[UserRegistration].query(email=email)[0]
+    if not hasattr(user, 'unconfirmed_dbs'):
+        unconfirmed_dbs = PersistentMapping()
+        user.unconfirmed_dbs = unconfirmed_dbs
+    else:
+        unconfirmed_dbs = user.unconfirmed_dbs
+
+    # Stripe information also needed - let's save in a dict XXX
+    unconfirmed_dbs[username] = password_hash
+
+    return {'ok': 1}
+
+
+@view_config(route_name='confirm_subdb', renderer='json')
+def confirm_subdb(request):
+    return {}
+
+
+@view_config(route_name='remove_subdb', renderer='json')
+def remove_subdb(request):
+    return {}
