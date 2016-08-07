6b96ccc0102c05670ebabf3269682b2b51e3306a
Michael Egorov <michael@zerodb.io>
2016-08-06 20:46:40 -0700

    Stripe in ini file

--- zerodb_dbaas/views/instances.py
+++ zerodb_dbaas/views/instances.py
@@ -98,7 +98,7 @@ def confirm_subdb(request):
     # XXX call stripe and if it is all good...
     # Set your secret key: remember to change this to your live secret key in production
     # See your keys here https://dashboard.stripe.com/account/apikeys
-    stripe.api_key = "sk_test_qiEtoPFg1UktBLSSBNHxCc38"
+    stripe.api_key = request.registry.settings['stripe.api_key']
 
     # Get the credit card details submitted by the form
     token = request.POST['stripeToken']
